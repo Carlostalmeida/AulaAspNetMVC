a265549732985765d7b30358bbf08ef616dc9868
Carlos Almeida <carlosmx7@gmail.com>
2023-01-09 23:04:34 -0300

    Primeiro Comite dos arquivos

---
+++ LanchesKing/LanchesKing/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesKing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesKing.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) :base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
---
+++ MVC2022/MVC2022/Startup.cs
@@ -0,0 +1,6 @@
+﻿namespace MVC2022
+{
+    public class Startup
+    {
+    }
+}
